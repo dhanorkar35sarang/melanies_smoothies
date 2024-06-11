50ec531b1550c9bc68ffb068d9ab95a76d0e0514
dhanorkar35sarang <dhanorkar35sarang@gmail.com>
2024-06-11 18:54:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,3 +65,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your smoothie is ordered, {name_on_order}!', icon="✅")
+
+# Example for testing with a specific fruit (optional)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
